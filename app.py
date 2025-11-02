5a254ec3f79a8b85f766b508d52b4c003e5f51a4
Meghna Sahu <meghnasahu63@gmail.com>
2025-11-02 15:14:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ from langchain_community.tools import ArxivQueryRun,WikipediaQueryRun,DuckDuckGo
 from langchain.agents import initialize_agent,AgentType
 from langchain_community.callbacks import StreamlitCallbackHandler
 import os
-from dotenv import load_dotenv
-load_dotenv()
+api_key = st.secrets["GROQ_API_KEY"]
 arxiv_wrapper=ArxivAPIWrapper(top_k_results=1,doc_content_chars_max=200)
 arxiv=ArxivQueryRun(api_wrapper=arxiv_wrapper)
 api_wrapper=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=200)
@@ -15,7 +14,7 @@ wiki=WikipediaQueryRun(api_wrapper=api_wrapper)
 search=DuckDuckGoSearchRun(name="search")
 st.title("🔍 Langchain-Chat With Search")
 st.sidebar.title("Settings")
-api_key=st.sidebar.text_input("Enter your Groq API Key: ",type="password")
+# api_key=st.sidebar.text_input("Enter your Groq API Key: ",type="password")
 if "messages" not in st.session_state:
     st.session_state["messages"]=[
         {
@@ -35,6 +34,7 @@ if prompt:=st.chat_input(placeholder="What is machine learning?"):
         response=search_agent.run(prompt,callbacks=[st_cb])
         st.session_state.messages.append({"role":'assistant',"content":response})
         st.write(response)
+
 
 
 
